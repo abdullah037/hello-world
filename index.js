524084be53f4d674594c17bfb740de3526acea0d
Abdullah <89657882+abdullah037@users.noreply.github.com>
2022-10-12 18:24:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Server running on http://65.0.19.114:${port}/`);
 });
